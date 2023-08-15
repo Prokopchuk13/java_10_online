02199606f4ddee778747db1a1be314b19753a9fb
Vitalii_Prokopchuk <1prokopchuk.vv@gmail.com>
2023-08-15 21:44:41 +0300

    level_2

---
+++ hw_1_compilation/Java/level_2/final_1/src/ua/com/alevel/Hello.java
@@ -0,0 +1,31 @@
+package ua.com.alevel;
+import ua.com.alevel.test.Message;
+import ua.com.alevel.depends.Depends;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        Message m = new Message();
+        byte b = Byte.MAX_VALUE;
+        byte b1 = Byte.MIN_VALUE;
+        short s = Short.MAX_VALUE;
+        short s1 = Short.MIN_VALUE;
+        int i = Integer.MAX_VALUE;
+        int i1 = Integer.MIN_VALUE;
+        long l = Long.MAX_VALUE;
+        long l1 = Long.MIN_VALUE;
+
+        m.print(String.valueOf(b));
+        m.print(String.valueOf(b1));
+        m.print(String.valueOf(s));
+        m.print(String.valueOf(s1));
+        m.print(String.valueOf(i));
+        m.print(String.valueOf(i1));
+        m.print(String.valueOf(l));
+        m.print(String.valueOf(l1));
+
+        System.out.println();
+        Depends d = new Depends();
+        d.console("Hello World");
+    }
+}
--- hw_1_compilation/Java/level_2/run.sh
+++
@@ -1,9 +0,0 @@
-echo 'start'
-
-cd ./final_1
-. ./run.sh
-
-cd ..
-
-cd ./final_2
-. ./run.sh
