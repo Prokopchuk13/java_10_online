75a9b552f5157c665e5da4b196cbee828fc0bd6f
Vitalii_Prokopchuk <1prokopchuk.vv@gmail.com>
2023-09-06 22:43:35 +0300

    hw_2

---
+++ hw_2_base_operations/Two.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Two {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter the value: ");
+
+        HashMap<String,Integer> letters = new HashMap<String, Integer>();
+
+        try {
+            String theline = bufferedReader.readLine();
+            char[] charArray = theline.toCharArray();
+            for (char c : charArray) {
+                if (String.valueOf(c).matches ("[a-zA-ZА-ЩЬЮЯҐЄІЇа-щьюяґєії]+\\.?")){
+                    if (!letters.containsKey(String.valueOf(c))){
+                        letters.put(String.valueOf(c),1);
+                    } else {
+                        letters.put(String.valueOf(c),letters.get(String.valueOf(c))+1);
+                    }
+                }
+            }
+            letters.forEach((key, value) -> System.out.println(key + " = " + value));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
